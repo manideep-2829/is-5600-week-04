fd6381f4fd2853f62022a84a1c96902ee2c333d0
manideep-2829 <manideep.yamsani@slu.edu>
2025-02-12 05:23:14 +0000

    Lab-04 Submission This is my submission for lab 04

--- app.js
+++ app.js
@@ -4,16 +4,37 @@ const express = require('express')
 
 // Set the port
 const port = process.env.PORT || 3000
+
 // Boot the app
 const app = express()
+
 // Register the public directory
+
+const api = require('./api')// Register the public directory
 app.use(express.static(__dirname + '/public'));
 // register the routes
 app.get('/products', listProducts)
 app.get('/', handleRoot);
+app.get('/products', api.listProducts)
+app.get('/', api.handleRoot);
+app.get('/products/:id', api.getProduct)
+
+app.delete('delete/:id',api.deleteProduct)
+app.put('update/:id',api.updateProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+const middleware = require('./middleware')
+
+// Register our upcoming middleware
+app.use(middleware.cors)
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.post('/products',api.createProduct)
+app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
 /**
  * Handle the root route
  * @param {object} req
@@ -22,7 +43,6 @@ app.listen(port, () => console.log(`Server listening on port ${port}`))
 function handleRoot(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 }
-
 /**
  * List all products
  * @param {object} req
